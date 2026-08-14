f57abb9e018fe366b8b2175de93f17fdf74c9572
Roalec <roalec@o2.pl>
2026-08-14 15:51:48 +0200

    feat: add login template

--- potato/potato/urls.py
+++ potato/potato/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
